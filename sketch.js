d27dd6fafa7c9b58df95ecba0e861ca5b485ae2e
Squishy <squishycaterpillar@gmail.com>
2019-01-07 21:14:45 -0500

    Testing #4

--- sketch.js
+++ sketch.js
@@ -1,47 +1,19 @@
-
-var rot = 0;
+var r, g, b;
 
 function setup() {
-  createCanvas(windowWidth/2, windowHeight/2);
-  noStroke();
-  angleMode("degrees");
-  stroke(0);
-  strokeWeight(2);
-  rectMode(CENTER);
-
-}
-
-function Cube(x, y) {
-  this.x = x || 400;
-
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth/2, windowHeight/2);
+  createCanvas(displayWidth, displayHeight);
+  r = random(50, 255);
+  g = random(0, 200);
+  b = random(50, 255);
 }
 
 function draw() {
-  // Background
-  background(200);
+  background(r, g, b);
+  console.log('draw');
+}
 
-  rot = accelerationX*100;
-  fill(0, 255, 0);
-  textSize(80);
-  //console.log(rotationX, rotationY, rotationZ);
-  if (rot === null) {
-    text("null...", 10, 100);
-  } else  {
-    text(rot, 10, 100);
-  }
-
-
-
-
-  fill(255);
-  push();
-    translate(width/2, height/2);
-    rotate(-rot);
-    rect(0, 0, 300, 300);
-  pop();
-
+function deviceMoved() {
+    r = map(accelerationX, -90, 90, 100, 175);
+    g = map(accelerationY, -90, 90, 100, 200);
+    b = map(accelerationZ, -90, 90, 100, 200);
 }
