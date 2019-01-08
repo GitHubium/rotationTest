7deff9e23fd0a455a268f8428b9bf89657227479
Squishy <squishycaterpillar@gmail.com>
2019-01-07 20:41:59 -0500

    Testing #3

--- mySketch.js
+++ mySketch.js
@@ -2,11 +2,12 @@
 var rot = 0;
 
 function setup() {
-  createCanvas(windowWidth/2, windowHeight/2, WEBGL);
+  createCanvas(windowWidth/2, windowHeight/2);
   noStroke();
   angleMode("degrees");
   stroke(0);
   strokeWeight(2);
+  rectMode(CENTER);
 
 }
 
@@ -16,18 +17,23 @@ function Cube(x, y) {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth/2, windowHeight/2, WEBGL);
+  resizeCanvas(windowWidth/2, windowHeight/2);
 }
 
 function draw() {
-  rot = accelerationX;
-  //console.log(rotationZ);
+  rot = accelerationX*100 || frameCount;
+  fill(0);
+  text(rot, windowWidth/2, windowHeight/2);
 
 
   // Background
   background(200);
 
-  rotateX(rot);
-  box(200, 200, 200);
+  fill(255);
+  push();
+    translate(width/2, height/2);
+    rotate(rot);
+    rect(0, 0, 300, 300);
+  pop();
 
 }
