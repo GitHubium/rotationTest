d4fc28fdfd6c2b387cfc8d407f18788b5a6e6d57
Squishy <squishycaterpillar@gmail.com>
2019-01-07 21:58:30 -0500

    Still testing #5

--- sketch.js
+++ sketch.js
@@ -1,19 +1,53 @@
-var r, g, b;
+
+var rot = 0;
+var absolute = 0;
+var alpha = 0;
+var beta = 0;
+var gamma = 0;
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
-  r = random(50, 255);
-  g = random(0, 200);
-  b = random(50, 255);
+  createCanvas(windowWidth/2, windowHeight/2);
+  noStroke();
+  angleMode("degrees");
+  stroke(0);
+  strokeWeight(2);
+  rectMode(CENTER);
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth/2, windowHeight/2);
 }
 
 function draw() {
-  background(r, g, b);
-  console.log('draw');
+  // Background
+  background(200);
+
+  rot = (alpha===null)?"null...":alpha;
+
+  fill(0, 255, 0);
+  textSize(80);
+  text(rot, 10, 100);
+
+
+
+
+  fill(255);
+  push();
+    translate(width/2, height/2);
+    rotate(rot);
+    rect(0, 0, 300, 300);
+  pop();
+
 }
 
-function deviceMoved() {
-    r = map(accelerationX, -90, 90, 100, 175);
-    g = map(accelerationY, -90, 90, 100, 200);
-    b = map(accelerationZ, -90, 90, 100, 200);
+function handleOrientation(event) {
+  absolute = event.absolute;
+  alpha    = event.alpha;
+  beta     = event.beta;
+  gamma    = event.gamma;
+
+  // Do stuff with the new orientation data
 }
+
+window.addEventListener("deviceorientation", handleOrientation, true);
